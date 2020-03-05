3ab05fa305483eb8d78ea1578cf73e34499f8630
ugis70194 <ugis70194@yahoo.co.jp>
2020-03-06 03:28:09 +0900

    storage: add tester solution by ugis

---
+++ storage/answer-ugis/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int A, N, K;
+    cin >> A >> N >> K;
+    vector<int> W(N+1);
+    for(int i = 1; i <= N; i++) cin >> W[i];
+
+    vector<int> dp(A+1, 1010);
+    dp[0] = 0;
+    for(int i = 1; i <= N; i++){
+        for(int w = A; w >= 0; w--){
+            if(w - W[i] < 0) continue;
+            dp[w] = min(dp[w], dp[w - W[i]] + 1);
+        }
+    }
+
+    if(dp[A] <= K) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
