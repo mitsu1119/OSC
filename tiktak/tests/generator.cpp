96551b605264f1a6e0bd926cc9aa482ce50d8e1c
mitsu1119 <hachimitsu1119@gmail.com>
2020-03-06 06:40:10 +0900

    generatorとvalidatorの定数を修正したフォーマットに従って修正

--- tiktak/tests/generator.cpp
+++ tiktak/tests/generator.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 using i64 = int_fast64_t;
-const i64 MIN_n = 0;
-const i64 MAX_n = 1e6;
+const i64 MIN_N = 0;
+const i64 MAX_N = 1e6;
 
 int main(int argc, char *argv[]) {
 	registerGen(argc, argv, 1);
 	for(i64 t = 0; t < 30; t++) {
 		ofstream of(format("02_random_%02d.in", t + 1).c_str());
-		of << rnd.next(MIN_n, MAX_n) << endl;
+		of << rnd.next(MIN_N, MAX_N) << endl;
 		of.close();
 	}
 
--- tiktak/tests/validator.cpp
+++ tiktak/tests/validator.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 using i64 = int_fast64_t;
-const i64 MIN_n = 0;
-const i64 MAX_n = 1e6;
+const i64 MIN_N = 0;
+const i64 MAX_N = 1e6;
 
 int main(int argc, char *argv[]) {
 	registerValidation(argc, argv);
-	inf.readInt(MIN_n, MAX_n, "n");
+	inf.readInt(MIN_N, MAX_N, "N");
 	inf.readEoln();
 	inf.readEof();
 	return 0;
